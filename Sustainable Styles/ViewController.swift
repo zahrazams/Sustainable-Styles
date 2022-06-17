f4c198e131d74576a400ad228cf6459ed10115c8
Judy <tianrui2054@gmail.com>
2022-06-17 08:57:50 -0400

    Finished quiz layout

--- Sustainable Styles/ViewController.swift
+++ Sustainable Styles/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func lbsWrong(_ sender: Any) {
+        
+    }
+    
 }
 
